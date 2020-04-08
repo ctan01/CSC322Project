83b73fb0403f4a7901b1824dbac1466c53c1d796
ctan01 <48540650+ctan01@users.noreply.github.com>
2020-04-08 18:04:30 -0400

    Update Login_Page.py

--- Login_Page.py
+++ Login_Page.py
@@ -1,47 +1,48 @@
-from tkinter import *
+import tkinter as tk
 
-root = Tk()
+root = tk.Tk()
+root.title("Login")
 
-topFrame = Frame(root)
-topFrame.pack(side=TOP)
-bottomFrame = Frame(root)
-bottomFrame.pack(side=BOTTOM)
+topFrame = tk.Frame(root)
+topFrame.pack(side=tk.TOP)
+bottomFrame = tk.Frame(root)
+bottomFrame.pack(side=tk.BOTTOM)
 
+# top menu bar
+menu = tk.Menu(root)
+root.config(menu=menu)
+
+Button_back = tk.Button(menu)
+menu.add_cascade(label="Back", menu=Button_back)
 
 # TOP FRAME
 
+Label_name = tk.Label(topFrame, text="Name").grid(row=0, sticky=tk.E)
+Label_password = tk.Label(topFrame, text="Password").grid(row=1, sticky=tk.E)
 
-label_1 = Label(topFrame, text="Name")
-label_2 = Label(topFrame, text="Password")
-entry_1 = Entry(topFrame)
-entry_2 = Entry(topFrame)
-c = Checkbutton(topFrame, text="Keep me logged in")
-
-label_1.grid(row=0, sticky=E)
-label_2.grid(row=1, sticky=E)
-entry_1.grid(row=0, column=1)
-entry_2.grid(row=1, column=1)
-c.grid(columnspan=2)
+Entry_name = tk.Entry(topFrame).grid(row=0, column=1)
+Entry_password = tk.Entry(topFrame).grid(row=1, column=1)
+Checkbutton_keepSignIn = tk.Checkbutton(topFrame, text="Keep me logged in").grid(columnspan=2)
 
 
 # BOTTOM FRAME
 
-def register(event):
+def register(event):  # jumps to register page
     print("register")
 
 
-button_1 = Button(bottomFrame, text="Register")
-button_1.bind("<Button-1>", register)
-button_1.pack(side=LEFT)
+Button_register = tk.Button(bottomFrame, text="Register")
+Button_register.bind("<Button-1>", register)  # "<Button-1>" means left click
+Button_register.pack(side=tk.LEFT)
 
 
-def logIn(event):
+def logIn(event):  # login with info entry
     print("Log in")
 
 
-button_2 = Button(bottomFrame, text="Log in")
-button_2.bind("<Button-1>", logIn)
-button_2.pack(side=RIGHT)
-
+Button_logIn = tk.Button(bottomFrame, text="Log in")
+Button_logIn.bind("<Button-1>", logIn)
+Button_logIn.pack(side=tk.RIGHT)
 
 root.mainloop()
+
