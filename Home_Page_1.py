14d179ba79096e85eb5c2f9257782b99dff203b6
ctan01 <48540650+ctan01@users.noreply.github.com>
2020-04-08 20:46:11 -0400

    Update Home_Page_1.py

--- Home_Page_1.py
+++ Home_Page_1.py
@@ -1,19 +1,30 @@
-from tkinter import *
-
-root = Tk()
-root.title("Home Page")
+import tkinter as tk
 
 
-menu = Menu(root)
-root.config(menu=menu)
-
-loginButton = Button(menu)
-menu.add_cascade(label="Login", menu=loginButton)
+def logIn():
+    print("jump to login page")
 
 
+class HomePage1:
+    height = 700
+    width = 800
 
-appName = Label(root, text="App Name", bg="green", fg="white")
-appName.grid(row=0, column=0, columnspan=10)
+    root = tk.Tk()
+    root.title("Home Page")
 
+    # top menu bar
+    menu = tk.Menu(root)
+    root.config(menu=menu)
 
-root.mainloop()
+    Button_logIn = tk.Button(menu)
+    menu.add_command(label="Login", command=logIn)
+
+    # main body
+    topFrame = tk.Frame(root, height=50, width=width)
+    topFrame.grid(row=0, column=0)
+    botFrame = tk.Frame(root, height=height, width=width)
+    botFrame.grid(row=1, column=0)
+
+    appName = tk.Label(topFrame, text="App Name", bg="green", fg="white").place(relwidth=1, relheight=1)
+
+    root.mainloop()
