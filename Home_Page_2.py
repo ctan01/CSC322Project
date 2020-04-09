89d25bcd7afa5f85cf2d25d516785c95418977f0
ctan01 <48540650+ctan01@users.noreply.github.com>
2020-04-08 20:47:05 -0400

    Update Home_Page_2.py

--- Home_Page_2.py
+++ Home_Page_2.py
@@ -1,27 +1,56 @@
-from tkinter import *
+import tkinter as tk
 
 
-def doNothing():
-    print("doNothing")
+def jumpToProfile(event):
+    print("jump to profile page")
 
 
-root = Tk()
-root.title("Home Page")
+def jumpToGroupPage():
+    print("jump to group page")
 
-menu = Menu(root)
-root.config(menu=menu)
 
-subMenu = Menu(menu)
-menu.add_cascade(label="Menu", menu=subMenu)
-subMenu.add_command(label="Profile", command=doNothing)
-subMenu.add_command(label="Your Groups", command=doNothing)
-subMenu.add_command(label="Inbox", command=doNothing)
-subMenu.add_command(label="Search User", command=doNothing)
+def jumpToInbox():
+    print("jump to inbox")
 
-logoutButton = Button(menu)
-menu.add_cascade(label="Logout", menu=logoutButton)
 
-appName = Label(root, text="App Name", bg="green", fg="white")
-appName.grid(row=0, column=0, columnspan=10)
+def jumpToUserSearch():
+    print("jump to user search")
 
-root.mainloop()
+
+def jumpToHomePage1():
+    print("log out and jump to home page 1")
+
+
+class HomePage2:
+    height = 700
+    width = 800
+
+    root = tk.Tk()
+    root.title("Home Page")
+
+    # menu bar
+
+    Menu_main = tk.Menu(root)
+    root.config(menu=Menu_main)
+
+    Menu_sub = tk.Menu(Menu_main)
+    Menu_main.add_cascade(label="Menu", menu=Menu_sub)
+    Menu_sub.add_command(label="Profile", command=jumpToProfile("<Button-1>"))
+    Menu_sub.add_command(label="Inbox", command=jumpToInbox)
+    Menu_sub.add_cascade(label="Your Groups", command=jumpToGroupPage)
+    Menu_sub.add_command(label="Search User", command=jumpToUserSearch)
+
+    logoutButton = tk.Button(Menu_main)
+    Menu_main.add_command(label="Logout", command=jumpToHomePage1)
+
+    # main body
+
+    topFrame = tk.Frame(root, height=50, width=width)
+    topFrame.grid(row=0, column=0)
+    botFrame = tk.Frame(root, height=height, width=width)
+    botFrame.grid(row=1, column=0)
+
+    appName = tk.Label(topFrame, text="App Name", bg="green", fg="white").place(relwidth=1, relheight=1)
+
+
+    root.mainloop()
