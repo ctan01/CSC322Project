f8837085387fac9234e1b51623d44e7ec5db04b3
meriselle <48763261+meriselleb@users.noreply.github.com>
2020-04-20 12:21:31 -0400

    Update Inbox_Page.py

--- Inbox_Page.py
+++ Inbox_Page.py
@@ -1,1 +1,25 @@
 
+import tkinter as tk
+
+# Inbox Page has the following:
+# invitations with accept or decline, accept all invitations, decline all invitations, log out 
+
+def backToHome(): 
+  print("back to home page")
+  
+def logOut():
+  print("log out")
+  
+  
+class InboxPage:
+  # set up always the same
+  root = tk.Tk()
+  root.title("Inbox")
+  
+  topFrame = tk.Frame(root)
+  topFrame.pack(side=tk.TOP)
+  bottomFrame = tk.Frame(root)
+  bottomFrame.pack(side = tk.BOTTOM)
+  
+  root.mainloop()
+  
