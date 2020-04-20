0f618efb1101b92dd2b90960328fb8641d5a03eb
meriselle <48763261+meriselleb@users.noreply.github.com>
2020-04-20 16:16:25 -0400

    Update userDatabase.py

--- userDatabase.py
+++ userDatabase.py
@@ -1,1 +1,10 @@
+# Database
+# Should have the following tables:
+# USER TABLE
+## with userID, username, user email, user password, user skills, user interests, user groups
+# GROUPS TABLE
+## groupID, groupname, group members
+
+
+
 
