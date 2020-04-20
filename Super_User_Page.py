d90c59ea0a55ddb880a61e5a3cbe78bec95c381d
meriselle <48763261+meriselleb@users.noreply.github.com>
2020-04-20 12:25:16 -0400

    Update Super_User_Page.py

--- Super_User_Page.py
+++ Super_User_Page.py
@@ -1,1 +1,25 @@
 import tkinter as tk
+
+# Super User Page has the following
+# log out, inbox, profile page, issue warning to user, view registration requests
+
+def backToHome():
+  print("back to home page")
+  
+def logOut():
+  print("log out")
+  
+def toInbox():
+  print("to inbox")
+  
+class SuperUserPage:
+  root = tk.Tk()
+  root.title("Super User Page")
+  
+  # set up, the same throughout
+  topFrame = tk.Frame(root)
+  topFrame.pack(side = tk.TOP)
+  bottomFrame = tk.Frame(root)
+  bottomFrame.pack(side = tk.BOTTOM)
+  
+  root.mainloop()
