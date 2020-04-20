1d634ef443e20501c7a48f5564fbc0c2ffb63c99
meriselle <48763261+meriselleb@users.noreply.github.com>
2020-04-20 12:11:18 -0400

    Update Group_Page.py

--- Group_Page.py
+++ Group_Page.py
@@ -1,1 +1,27 @@
+import tkinter as tk
 
+def backToHome():
+   print("Back to Home Page") # jump to home page 2
+
+def register():
+  print("Register") # register page
+  
+def logIn(): # login pop up
+  print("Log in")
+  
+def toInbox():
+  print("Inbox")
+  
+  
+class groupPage:
+  root = tk.Tk()
+  root.title ("Group Page")
+  
+  # set up : kept consistent through all the pages
+  topFrame = tk.Frame(root)
+  topFrame.pack(side=tk.TOP)
+  bottomFrame = tk.Frame(root)
+  bottomFrame.pack(side=tk.BOTTOM)
+  
+  
+  root.mainloop()
