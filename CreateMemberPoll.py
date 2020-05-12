f4c323f7344cf795dadf9247ea4075371bea5a49
meriselleb <meri.selle@yahoo.com>
2020-05-12 19:29:03 -0400

    Now can create member poll"

--- CreateMemberPoll.py
+++ CreateMemberPoll.py
@@ -1,20 +1,38 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 import pandas as pd
+from PyQt5.QtWidgets import QMessageBox
 
 
 class Ui_MemberPoll(object):
-    def setupUi(self, CreateMemberPoll):
-        CreateMemberPoll.setObjectName("CreateMemberPoll")
-        CreateMemberPoll.resize(386, 300)
-        self.centralwidget = QtWidgets.QWidget(CreateMemberPoll)
+
+    def submitMemberPoll(self):
+        df = pd.read_csv('MemberPoll.csv')
+        # MemberPollID,GroupID,AffectedMemberID,Type,Yes,No,Total
+        new_row = {'MemberPollID': (len(df.index)+1),
+            'GroupID' : -1, # UNKOWN FOR NOW (LINK WITH GROUP ID VALUE == 1)
+            'AffectedMemberID' : -1, # MATCH WITH FIRST NAME
+            'Type' : "FILL",
+            'Yes' : 0,
+            'No' : 0,
+            'Total' : 0
+        }
+        df = df.append(new_row, ignore_index=True)
+        df.to_csv('Posts.csv', index = False)
+
+        msg = QMessageBox()
+        msg.setWindowTitle("Publish Poll")
+        msg.setText("Poll has been published! Wait for at least 60% of Members to Respond before Results!")
+        x = msg.exec_()
+
+
+    def setupUi(self, MemberPoll):
+        MemberPoll.setObjectName("MemberPoll")
+        MemberPoll.resize(400, 300)
+        self.centralwidget = QtWidgets.QWidget(MemberPoll)
         self.centralwidget.setObjectName("centralwidget")
-        self.buttonBox = QtWidgets.QDialogButtonBox(CreateMemberPoll)
-        self.buttonBox.setGeometry(QtCore.QRect(90, 220, 201, 32))
-        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
-        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
-        self.buttonBox.setObjectName("buttonBox")
-        self.comboBox = QtWidgets.QComboBox(CreateMemberPoll)
-        self.comboBox.setGeometry(QtCore.QRect(60, 60, 271, 22))
+
+        self.comboBox = QtWidgets.QComboBox(MemberPoll)
+        self.comboBox.setGeometry(QtCore.QRect(60, 180, 271, 22))
         self.comboBox.setObjectName("comboBox")
         self.comboBox.addItem("")
         self.comboBox.addItem("")
@@ -24,26 +42,35 @@ class Ui_MemberPoll(object):
         self.comboBox.addItem("")
         self.comboBox.addItem("")
         
-        self.textBrowser = QtWidgets.QTextBrowser(CreateMemberPoll)
-        self.textBrowser.setGeometry(QtCore.QRect(20, 10, 351, 31))
+        self.textBrowser = QtWidgets.QTextBrowser(MemberPoll)
+        self.textBrowser.setGeometry(QtCore.QRect(20, 50, 351, 28))
         self.textBrowser.setObjectName("textBrowser")
-        self.comboBox_2 = QtWidgets.QComboBox(CreateMemberPoll)
-        self.comboBox_2.setGeometry(QtCore.QRect(60, 150, 271, 22))
+        self.comboBox_2 = QtWidgets.QComboBox(MemberPoll)
+        self.comboBox_2.setGeometry(QtCore.QRect(60, 85, 271, 22))
         self.comboBox_2.setObjectName("comboBox_2")
         self.comboBox_2.addItem("")
         self.comboBox_2.addItem("")
         self.comboBox_2.addItem("")
-        self.textBrowser_2 = QtWidgets.QTextBrowser(CreateMemberPoll)
-        self.textBrowser_2.setGeometry(QtCore.QRect(20, 100, 351, 31))
+        self.textBrowser_2 = QtWidgets.QTextBrowser(MemberPoll)
+        self.textBrowser_2.setGeometry(QtCore.QRect(20, 145, 351, 28))
         self.textBrowser_2.setObjectName("textBrowser_2")
 
-        self.retranslateUi(CreateMemberPoll)
-        QtCore.QMetaObject.connectSlotsByName(CreateMemberPoll)
+        self.PublishPostButton = QtWidgets.QPushButton(self.centralwidget)
+        self.PublishPostButton.setGeometry(QtCore.QRect(10, 0, 80, 30))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        self.PublishPostButton.setFont(font)
+        self.PublishPostButton.setObjectName("Publish")
+        self.PublishPostButton.clicked.connect(self.submitMemberPoll) # BEGIN TO PUBLISH POST
+        self.PublishPostButton.clicked.connect(MemberPoll.close)
 
-    def retranslateUi(self, CreateMemberPoll):
+        self.retranslateUi(MemberPoll)
+        QtCore.QMetaObject.connectSlotsByName(MemberPoll)
+
+    def retranslateUi(self, MemberPoll):
         _translate = QtCore.QCoreApplication.translate
-        CreateMemberPoll.setWindowTitle(_translate("CreateMemberPoll", "CreateMemberPoll"))
-        self.textBrowser.setHtml(_translate("CreateMemberPoll", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+        MemberPoll.setWindowTitle(_translate("MemberPoll", "MemberPoll"))
+        self.textBrowser.setHtml(_translate("MemberPoll", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
@@ -69,19 +96,20 @@ class Ui_MemberPoll(object):
                 userSix = userSix + row['Member6']
                 userSeven = userSeven + row['Member7']
        
-        self.comboBox.setItemText(0, _translate("CreateMemberPoll", userZero)) 
-        self.comboBox.setItemText(1, _translate("CreateMemberPoll", userOne))       
-        self.comboBox.setItemText(2, _translate("CreateMemberPoll", userTwo))       
-        self.comboBox.setItemText(3, _translate("CreateMemberPoll", userThree))       
-        self.comboBox.setItemText(4, _translate("CreateMemberPoll", userFour))       
-        self.comboBox.setItemText(5, _translate("CreateMemberPoll", userFive))       
-        self.comboBox.setItemText(6, _translate("CreateMemberPoll", userSix))       
-        self.comboBox.setItemText(7, _translate("CreateMemberPoll", userSeven))       
+        self.comboBox.setItemText(0, _translate("MemberPoll", userZero)) 
+        self.comboBox.setItemText(1, _translate("MemberPoll", userOne))       
+        self.comboBox.setItemText(2, _translate("MemberPoll", userTwo))       
+        self.comboBox.setItemText(3, _translate("MemberPoll", userThree))       
+        self.comboBox.setItemText(4, _translate("MemberPoll", userFour))       
+        self.comboBox.setItemText(5, _translate("MemberPoll", userFive))       
+        self.comboBox.setItemText(6, _translate("MemberPoll", userSix))       
+        self.comboBox.setItemText(7, _translate("MemberPoll", userSeven))       
 
-        self.comboBox_2.setItemText(0, _translate("CreateMemberPoll", "Vote to Send Warning to Member"))
-        self.comboBox_2.setItemText(1, _translate("CreateMemberPoll", "Vote to Kick Member Out"))
-        self.comboBox_2.setItemText(2, _translate("CreateMemberPoll", "Vote to Send Compliment to Member"))
-        self.textBrowser_2.setHtml(_translate("CreateMemberPoll", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+        self.comboBox_2.setItemText(0, _translate("MemberPoll", "Vote to Send Warning to Member"))
+        self.comboBox_2.setItemText(1, _translate("MemberPoll", "Vote to Kick Member Out"))
+        self.comboBox_2.setItemText(2, _translate("MemberPoll", "Vote to Send Compliment to Member"))
+        self.PublishPostButton.setText(_translate("CreatePost", "Publish"))
+        self.textBrowser_2.setHtml(_translate("MemberPoll", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
@@ -91,8 +119,8 @@ class Ui_MemberPoll(object):
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    CreateMemberPoll = QtWidgets.QMainWindow()
+    MemberPoll = QtWidgets.QMainWindow()
     ui = Ui_MemberPoll()
-    ui.setupUi(CreateMemberPoll)
-    CreateMemberPoll.show()
+    ui.setupUi(MemberPoll)
+    MemberPoll.show()
     sys.exit(app.exec_())
