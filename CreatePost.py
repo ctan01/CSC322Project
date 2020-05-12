fe04555bb581ba344e5567066d12ff5a5f99b836
meriselleb <meri.selle@yahoo.com>
2020-05-12 15:30:16 -0400

    can now post to Posts.csv

--- CreatePost.py
+++ CreatePost.py
@@ -6,6 +6,20 @@ import pandas as pd
 class Ui_CreatePost(object):
     def publishPost(self):
         df = pd.read_csv('Posts.csv')
+        
+        # part that will check which groupID currentGroup has value 1
+
+        # PostID, GroupID, PostContents, Comment0, Comment1, Comment2,Comment3
+        new_row = {'PostID': len(df.index),
+            'GroupID' : 'UNK',
+            'PostContents': self.lineEdit_Postcontents.text(),
+            'Comment0': None,
+            'Comment1': None,
+            'Comment2': None,
+            'Comment3': None
+            }
+        df = df.append(new_row, ignore_index=True)
+        df.to_csv('Posts.csv', index= False)
 
         msg = QMessageBox()
         msg.setWindowTitle("Publish Post")
@@ -19,9 +33,9 @@ class Ui_CreatePost(object):
         self.centralwidget.setObjectName("centralwidget")
         
 
-        self.lineEdit_firstName = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit_firstName.setGeometry(QtCore.QRect(10,10,300,30))
-        self.lineEdit_firstName.setObjectName("lineEdit_firstName")
+        self.lineEdit_Postcontents = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_Postcontents.setGeometry(QtCore.QRect(10,10,300,30))
+        self.lineEdit_Postcontents.setObjectName("lineEdit_Postcontents")
 
         self.PublishPostButton = QtWidgets.QPushButton(self.centralwidget)
         self.PublishPostButton.setGeometry(QtCore.QRect(60, 50, 200, 25))
