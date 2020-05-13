2cccd3ce7d9670bffd2f4e41e4c416ba5f97d823
meriselleb <meri.selle@yahoo.com>
2020-05-13 03:43:11 -0400

    inbox progress

--- InboxPage.py
+++ InboxPage.py
@@ -72,6 +72,8 @@ class Ui_InboxPage(object):
                 self.GroupsButton.setObjectName("GroupsButton")
                 self.GroupsButton.clicked.connect(self.openGroupsPage)
 
+                # MESSAGE BOXES
+
                 self.groupBox_2 = QtWidgets.QGroupBox(self.InboxDashboard)              
                 self.groupBox_2.setGeometry(QtCore.QRect(10, 0, 211, 61))
                 self.groupBox_2.setObjectName("groupBox_2")
@@ -81,9 +83,13 @@ class Ui_InboxPage(object):
                 self.HomeButton_2 = QtWidgets.QPushButton(self.groupBox_2)
                 self.HomeButton_2.setGeometry(QtCore.QRect(140, 20, 61, 31))
                 self.HomeButton_2.setObjectName("HomeButton_2")
+
+
                 self.listWidget = QtWidgets.QListWidget(self.InboxDashboard)
                 self.listWidget.setGeometry(QtCore.QRect(10, 70, 1031, 711))
                 self.listWidget.setObjectName("listWidget")
+
+
                 self.InvitationMessage = QtWidgets.QWidget(self.InboxDashboard)
                 self.InvitationMessage.setGeometry(QtCore.QRect(20, 80, 1011, 101))
                 self.InvitationMessage.setObjectName("InvitationMessage")
@@ -93,42 +99,73 @@ class Ui_InboxPage(object):
                 self.pushButton = QtWidgets.QPushButton(self.InvitationMessage)
                 self.pushButton.setGeometry(QtCore.QRect(10, 70, 93, 28))
                 self.pushButton.setObjectName("pushButton")
-                self.pushButton_2 = QtWidgets.QPushButton(self.InvitationMessage)
-                self.pushButton_2.setGeometry(QtCore.QRect(110, 70, 93, 28))
-                self.pushButton_2.setObjectName("pushButton_2")
+                self.Decline2 = QtWidgets.QPushButton(self.InvitationMessage)
+                self.Decline2.setGeometry(QtCore.QRect(110, 70, 93, 28))
+                self.Decline2.setObjectName("Decline2")
+
+
                 self.InvitationMessage_2 = QtWidgets.QWidget(self.InboxDashboard)
                 self.InvitationMessage_2.setGeometry(QtCore.QRect(20, 190, 1011, 101))
                 self.InvitationMessage_2.setObjectName("InvitationMessage_2")
                 self.textBrowser_2 = QtWidgets.QTextBrowser(self.InvitationMessage_2)
                 self.textBrowser_2.setGeometry(QtCore.QRect(10, 10, 991, 51))
                 self.textBrowser_2.setObjectName("textBrowser_2")
-                self.pushButton_3 = QtWidgets.QPushButton(self.InvitationMessage_2)
-                self.pushButton_3.setGeometry(QtCore.QRect(10, 70, 93, 28))
-                self.pushButton_3.setObjectName("pushButton_3")
-                self.pushButton_4 = QtWidgets.QPushButton(self.InvitationMessage_2)
-                self.pushButton_4.setGeometry(QtCore.QRect(110, 70, 93, 28))
-                self.pushButton_4.setObjectName("pushButton_4")
-                self.NotificationWidget = QtWidgets.QWidget(self.InboxDashboard)
-                self.NotificationWidget.setGeometry(QtCore.QRect(20, 300, 1011, 101))
-                self.NotificationWidget.setObjectName("NotificationWidget")
-                self.textBrowser_3 = QtWidgets.QTextBrowser(self.NotificationWidget)
-                self.textBrowser_3.setGeometry(QtCore.QRect(10, 10, 991, 51))
-                self.textBrowser_3.setObjectName("textBrowser_3")
-                self.pushButton_5 = QtWidgets.QPushButton(self.NotificationWidget)
-                self.pushButton_5.setGeometry(QtCore.QRect(10, 70, 93, 28))
-                self.pushButton_5.setObjectName("pushButton_5")
-                self.PlainNotification = QtWidgets.QWidget(self.InboxDashboard)
-                self.PlainNotification.setGeometry(QtCore.QRect(20, 410, 1011, 81))
-                self.PlainNotification.setObjectName("PlainNotification")
-                self.textBrowser_4 = QtWidgets.QTextBrowser(self.PlainNotification)
-                self.textBrowser_4.setGeometry(QtCore.QRect(10, 10, 991, 61))
-                self.textBrowser_4.setObjectName("textBrowser_4")
-                self.pushButton_6 = QtWidgets.QPushButton(self.InboxDashboard)
-                self.pushButton_6.setGeometry(QtCore.QRect(740, 740, 141, 28))
-                self.pushButton_6.setObjectName("pushButton_6")
-                self.pushButton_7 = QtWidgets.QPushButton(self.InboxDashboard)
-                self.pushButton_7.setGeometry(QtCore.QRect(890, 740, 141, 28))
-                self.pushButton_7.setObjectName("pushButton_7")
+                self.Accept3 = QtWidgets.QPushButton(self.InvitationMessage_2)
+                self.Accept3.setGeometry(QtCore.QRect(10, 70, 93, 28))
+                self.Accept3.setObjectName("Accept3")
+                self.Decline3 = QtWidgets.QPushButton(self.InvitationMessage_2)
+                self.Decline3.setGeometry(QtCore.QRect(110, 70, 93, 28))
+                self.Decline3.setObjectName("Decline3")
+
+                # NEW MESG
+                self.InvitationMessage3 = QtWidgets.QWidget(self.InboxDashboard)
+                self.InvitationMessage3.setGeometry(QtCore.QRect(20, 300, 1011, 101))
+                self.InvitationMessage3.setObjectName("InvitationMessage3")
+                self.textBrowser3 = QtWidgets.QTextBrowser(self.InvitationMessage3)
+                self.textBrowser3.setGeometry(QtCore.QRect(10, 10, 991, 51))
+                self.textBrowser3.setObjectName("textBrowser3")
+                self.pushButton43 = QtWidgets.QPushButton(self.InvitationMessage3)
+                self.pushButton43.setGeometry(QtCore.QRect(10, 70, 93, 28))
+                self.pushButton43.setObjectName("pushButton43")
+                self.Decline43 = QtWidgets.QPushButton(self.InvitationMessage3)
+                self.Decline43.setGeometry(QtCore.QRect(110, 70, 93, 28))
+                self.Decline43.setObjectName("Decline43")
+
+                self.InvitationMessage4 = QtWidgets.QWidget(self.InboxDashboard)
+                self.InvitationMessage4.setGeometry(QtCore.QRect(20, 410, 1011, 101))
+                self.InvitationMessage4.setObjectName("InvitationMessage4")
+                self.textBrowser4 = QtWidgets.QTextBrowser(self.InvitationMessage4)
+                self.textBrowser4.setGeometry(QtCore.QRect(10, 10, 991, 51))
+                self.textBrowser4.setObjectName("textBrowser4")
+                self.pushButton4 = QtWidgets.QPushButton(self.InvitationMessage4)
+                self.pushButton4.setGeometry(QtCore.QRect(10, 70, 93, 28))
+                self.pushButton4.setObjectName("pushButton4")
+                self.Decline4 = QtWidgets.QPushButton(self.InvitationMessage4)
+                self.Decline4.setGeometry(QtCore.QRect(110, 70, 93, 28))
+                self.Decline4.setObjectName("Decline4")
+
+                self.InvitationMessage5 = QtWidgets.QWidget(self.InboxDashboard)
+                self.InvitationMessage5.setGeometry(QtCore.QRect(20, 520, 1011, 101))
+                self.InvitationMessage5.setObjectName("InvitationMessage5")
+                self.textBrowser5 = QtWidgets.QTextBrowser(self.InvitationMessage5)
+                self.textBrowser5.setGeometry(QtCore.QRect(10, 10, 991, 51))
+                self.textBrowser5.setObjectName("textBrowser5")
+                self.pushButton5 = QtWidgets.QPushButton(self.InvitationMessage5)
+                self.pushButton5.setGeometry(QtCore.QRect(10, 70, 93, 28))
+                self.pushButton5.setObjectName("pushButton5")
+                self.Decline5 = QtWidgets.QPushButton(self.InvitationMessage5)
+                self.Decline5.setGeometry(QtCore.QRect(110, 70, 93, 28))
+                self.Decline5.setObjectName("Decline5")
+
+
+
+                self.AcceptALL = QtWidgets.QPushButton(self.InboxDashboard)
+                self.AcceptALL.setGeometry(QtCore.QRect(740, 740, 141, 28))
+                self.AcceptALL.setObjectName("AcceptALL")
+
+                self.DeclineALL = QtWidgets.QPushButton(self.InboxDashboard)
+                self.DeclineALL.setGeometry(QtCore.QRect(890, 740, 141, 28))
+                self.DeclineALL.setObjectName("DeclineALL")
                 InboxPage.setCentralWidget(self.InboxDashboard)
                 self.menubar = QtWidgets.QMenuBar(InboxPage)
                 self.menubar.setGeometry(QtCore.QRect(0, 0, 1052, 26))
@@ -158,28 +195,41 @@ class Ui_InboxPage(object):
         "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'MS Shell Dlg 2\';\">Invitation to Join Beach Clean Up</span></p>\n"
         "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:\'MS Shell Dlg 2\';\"><br /></p></body></html>"))
                 self.pushButton.setText(_translate("InboxPage", "Accept"))
-                self.pushButton_2.setText(_translate("InboxPage", "Decline"))
+                self.Decline2.setText(_translate("InboxPage", "Decline"))
                 self.textBrowser_2.setHtml(_translate("InboxPage", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
         "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
         "p, li { white-space: pre-wrap; }\n"
         "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
         "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'MS Shell Dlg 2\';\">Invitation to Join Animal Shelter Volunteer Group</span></p></body></html>"))
-                self.pushButton_3.setText(_translate("InboxPage", "Accept"))
-                self.pushButton_4.setText(_translate("InboxPage", "Decline"))
-                self.textBrowser_3.setHtml(_translate("InboxPage", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+                self.Accept3.setText(_translate("InboxPage", "Accept"))
+                self.Decline3.setText(_translate("InboxPage", "Decline"))
+
+                self.textBrowser3.setHtml(_translate("InboxPage", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
         "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
         "p, li { white-space: pre-wrap; }\n"
         "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
-        "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'MS Shell Dlg 2\';\">Community Garden Set Up a Meeting Time Poll</span></p>\n"
-        "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:\'MS Shell Dlg 2\';\"><br /></p></body></html>"))
-                self.pushButton_5.setText(_translate("InboxPage", "Go to Page"))
-                self.textBrowser_4.setHtml(_translate("InboxPage", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+        "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'MS Shell Dlg 2\';\">Invitation to Join Animal Shelter Volunteer Group</span></p></body></html>"))
+                self.pushButton43.setText(_translate("InboxPage", "Accept"))
+                self.Decline43.setText(_translate("InboxPage", "Decline"))
+
+                self.textBrowser4.setHtml(_translate("InboxPage", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
         "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
         "p, li { white-space: pre-wrap; }\n"
         "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
-        "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'MS Shell Dlg 2\';\">You\'ve Been Issued a Warning by the Members of Community Garden</span></p></body></html>"))
-                self.pushButton_6.setText(_translate("InboxPage", "Accept All Invitations"))
-                self.pushButton_7.setText(_translate("InboxPage", "Decline All Invitations"))
+        "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'MS Shell Dlg 2\';\">Invitation to Join Animal Shelter Volunteer Group</span></p></body></html>"))
+                self.pushButton4.setText(_translate("InboxPage", "Accept"))
+                self.Decline4.setText(_translate("InboxPage", "Decline"))
+
+                self.textBrowser5.setHtml(_translate("InboxPage", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+        "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+        "p, li { white-space: pre-wrap; }\n"
+        "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
+        "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'MS Shell Dlg 2\';\">Invitation to Join Animal Shelter Volunteer Group</span></p></body></html>")) 
+                self.pushButton5.setText(_translate("InboxPage", "Accept"))
+                self.Decline5.setText(_translate("InboxPage", "Decline"))
+
+                self.AcceptALL.setText(_translate("InboxPage", "Accept All Invitations"))
+                self.DeclineALL.setText(_translate("InboxPage", "Decline All Invitations"))
 
 
 if __name__ == "__main__":
